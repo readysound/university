a6eaf87ddb824cd900ac545ec184c93572201389
miichiimusic <leonardo.ortegon@gmail.com>
2024-06-19 17:23:57 -0400

    logo svg

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,27 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from 'react';
+import { DocsThemeConfig } from 'nextra-theme-docs';
+
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
-  },
-}
+logo: (
+<>
+<img src="/logo.svg" alt="Readysound University Logo" width="24" height="24" />
+<span style={{ marginLeft: '.4em', fontWeight: 500 }}>
+Readysound University
+</span>
+</>
+),
+project: {
+link: 'https://github.com/shuding/nextra-docs-template',
+},
+chat: {
+link: 'https://discord.com',
+},
+docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+footer: {
+text: 'Nextra Docs Template',
+},
+};
 
-export default config
+
+export default config;
